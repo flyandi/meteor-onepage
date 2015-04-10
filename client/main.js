95a2d19b0421bc04b4deb7fd910507c9a92700a8
Andreas Schwarz <flyandi@yahoo.com>
2015-04-10 09:51:46 -0700

    Work

--- client/main.js
+++ client/main.js
@@ -1,14 +1,27 @@
+/**
+ * Meteor One Page Template
+ *
+ * The super simple and awesome looking One Page Website Template for Meteor
+ *
+ */
 
 
-
-
+/**
+ * ::globals
+ */
 
 UI.registerHelper("navigationLink", function(label) {
 	return label.replace(/\W+/g, "").toLowerCase();
 });
 
+UI.registerHelper("getSetting", function(name, _default) {
+	return Settings[name] || _default;
+});
 
 
+/**
+ * ::navigation
+ */
 
 Template.navigation.helpers({
 
@@ -23,19 +36,33 @@ Template.navigation.helpers({
 });
 
 
+/**
+ * ::content
+ */
 
 Template.content.helpers({
 
 	contentSections: function() {
-
 		return Settings.sections;
-		
 	},
 
-
 	isTemplate: function() {
-
 		return Template[this.template];
-	}
+	},
 
 })
+
+
+/**
+ * ::slider
+ */
+
+Template.slider.helpers({
+
+
+	getSlides: function() {	
+
+		return Settings.sliders[this.name] || false;
+	},	
+
+});
--- client/settings.js
+++ client/settings.js
@@ -1,23 +1,45 @@
-
 /**
- * Website Settings
+ * Meteor One Page Settings
  */
-
 
 Settings = {
 
+
+	/*
+	 * (logo) Specify a logo for this one page
+	 *
+	 * Recommendation: Use a SVG for maximum compatibility
+	 */
+
+	logo: '/logo.svg',
+
+	copyright: 'Copy &copy; 2015',
+
 	sections: [
 
-		{title: 'The Difference', headline: 'Making a difference'},
+		{title: 'Welcome', template: 'welcome'},
 
-
-		{title: 'Consulting'},
+		{title: 'Products'},
 
 		{title: 'About Us'},
 
 		{title: 'Contact Us'},
 
-	]
+	],
+
+
+	/** 
+	 * Goodies
+	 */
+
+	sliders: {
+
+		top: [
+			{image: '/slider.jpg', text: 'Even has an built in slider'},
+			{image: '/slider.jpg', text: 'This is simple to configure'},
+			{image: '/slider.jpg', text: 'And doesn\'t take anytime to get running'},
+		],
+	}
 
 
 };
